3caa9cd36b3ee25b81d0e2baa8882fad2b09caaf
chuckalbatross <jbonner89@gmail.com>
2017-09-19 10:38:25 -0700

    Implement functional get request with node-style callbacks

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,9 +4,16 @@ module.exports = {
   messages: {
     get: function (req, res) {
       if (req.method === 'GET') {
-        var messagesArr = models.messages.get();
-        console.log(messagesArr);
-        res.end(JSON.stringify(messagesArr));
+        var messagesArr = models.messages.get(function(err, content) {
+          if (err) {
+            console.log('ERROR:', err);
+          } else {
+            res.writeHead(200);
+            console.log(content);
+            res.end(JSON.stringify(content));
+          }
+        });
+        // res.end(JSON.stringify(messagesArr));
       }
     }, // a function which handles a get request for all messages
     post: function (req, res) {} // a function which handles posting a message to the database
--- server/models/index.js
+++ server/models/index.js
@@ -3,33 +3,30 @@ var promise = require('bluebird');
 
 module.exports = {
   messages: {
-    get: function () {   
-      return new promise((resolve, reject) => {
-        db.connection.query(`SELECT messages.text, messages.room, users.name 
-          FROM messages INNER JOIN users 
-          WHERE messages.user_id = users.user_id`,
-          (err, results) => {
-            if (err) {
-              console.log(`Error SELECTing: ${err}`);
-              reject(err);
-            } else {
-              var outputArr = [];
-              for (var i = 0; i < results.length; i++) {
-                var messageObj = {};
-                messageObj.text = results[i].text;
-                messageObj.name = results[i].name;
-                messageObj.room = results[i].room;
-                outputArr.push(messageObj);
-              }
-              resolve(outputArr);
-              //console.log(`RESULTS: ${JSON.stringify(results)}`);
+    get: function (callback) {   
+      db.connection.query(`SELECT messages.text, messages.room, users.name 
+        FROM messages INNER JOIN users 
+        WHERE messages.user_id = users.id`,
+        (err, results) => {
+          if (err) {
+            console.log(`Error SELECTing: ${err}`);
+          } else {
+            var outputArr = [];
+            for (var i = 0; i < results.length; i++) {
+              var messageObj = {};
+              messageObj.text = results[i].text;
+              messageObj.name = results[i].name;
+              messageObj.room = results[i].room;
+              outputArr.push(messageObj);
             }
-          });
-      }).then(value => {
-        result = value;
-      });
-    }, // a function which produces all the messages
+            console.log('Before callback:', outputArr);
+            callback(null, outputArr);
+          }
+        });
+    },
+    // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
+
   },
 
   users: {
